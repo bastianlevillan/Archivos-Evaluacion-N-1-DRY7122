e735646c4d080507ec784e7463d6189c98811f2f
bastianlevillan <bastianlevillan@github.com>
2023-04-05 03:43:47 +0000

    agregando script1 a carpeta

---
+++ script1.py
@@ -0,0 +1,5 @@
+print("Evaluación N°1 Programación y Redes Virtualizadas")
+print("Integrantes:")
+print("- Bastian Levillan")
+print("- Bastian Muñoz")
+print("- Braian Manriquez")
---
+++ script3.py
@@ -0,0 +1,14 @@
+numero_acl = input("Ingrese el número de ACL IPv4: ")
+
+# Determinar si el número de ACL es Estándar, Extendida o no existe
+if numero_acl.isdigit():
+    numero_acl = int(numero_acl)
+    if numero_acl >= 1 and numero_acl <= 99:
+        print("El número de ACL " + str(numero_acl) + " es una ACL Estándar.")
+    elif numero_acl >= 100 and numero_acl <= 199:
+        print("El número de ACL " + str(numero_acl) + " es una ACL Extendida.")
+    else:
+        print("El número de ACL " + str(numero_acl) + " no corresponde a una lista de acceso.")
+else:
+    print("El valor ingresado no es un número de ACL válido.")
+
